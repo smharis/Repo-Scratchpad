10bfac62df7c83911e26e22e5834568f4efc94bd
smharis <31839756+smharis@users.noreply.github.com>
2017-09-17 20:22:15 +0530

    Update my_editor.c

--- my_editor.c
+++ my_editor.c
@@ -2,19 +2,29 @@
 #include <stdio.h>
 #include <string.h>
 
+/*Macros*/
+#define INSERT 1
+#define DELETE 2
+#define UPDATE 3
+#deine  PRINT  4
+/*Struct to maintain stack framework*/
 typedef struct stack {
     struct stack *next;
+    
+    /*Type: either INSERT OR DELETE*/
     unsigned int type;
-    char *data;
+    
+    /*It will be holding data to be deleted or inserete depending upon type*/
+    char *data; 
 }Stack;
 
-void push (Stack **S, unsigned int type, char *data)
-{
+/*API to push each operation*/
+void
+push (Stack **S, unsigned int type, char *data) {
     Stack *s;
     s = *S;
     Stack *new1 = NULL;
-    if (!s)
-    {
+    if (!s) {
         s = (Stack *)malloc(sizeof(Stack));
         s->next = NULL;
         s->type = type;
@@ -30,8 +40,9 @@ void push (Stack **S, unsigned int type, char *data)
     return;
 }
 
-Stack *pop (Stack **S)
-{
+/*API to perform a pop of operation once user do undo*/
+Stack*
+pop (Stack **S) {
     Stack *s = *S;
     Stack *temp;
     if (!s)
@@ -39,22 +50,26 @@ Stack *pop (Stack **S)
     temp = s;
     s = s->next;
     *S = s;
-    return temp;
-   
+    return temp; 
 }
-
-void undo(Stack **S, char **my_texteditor)
-{
+ /*
+ **********************************************
+ * API will undo the operation by user.       *
+ * It will update the current file pointer    *
+ * and free the buffer created for stack node *
+ * followed by freeing of stack node also     *
+ **********************************************
+ */
+void
+undo(Stack **S, char **my_texteditor) {
     Stack *undo_oper = NULL;
     undo_oper = pop (S);
     unsigned int del;
-    if (undo_oper && undo_oper->type == 1)
-    {
+    if (undo_oper && undo_oper->type == INSERT) {
         /*perform delete*/
         (*my_texteditor)--;
         del = strlen(undo_oper->data);
-        while(del)
-          {
+        while(del) {
             **my_texteditor = '\0';
             (*my_texteditor)--;
             del--;
@@ -63,8 +78,7 @@ void undo(Stack **S, char **my_texteditor)
         free(undo_oper->data);
         free(undo_oper);
     }
-    else if (undo_oper && undo_oper->type == 2)
-    {
+    else if (undo_oper && undo_oper->type == DELETE) {
         /*peform insert*/
         del = strlen(undo_oper->data);
         strcpy(*my_texteditor,undo_oper->data);
@@ -75,12 +89,17 @@ void undo(Stack **S, char **my_texteditor)
     return;
 }
 
-char *insert (char **my_texteditor)
-{
+/*
+ *************************************************************
+ * API will put the data in editor and return the buffer     *
+ * to be pushed to stack framework                           *
+ *************************************************************
+ */
+char *
+insert (char **my_texteditor) {
     char  *temp = *my_texteditor;
     unsigned int len=0;
-    while (**my_texteditor != '\0')
-    {
+    while (**my_texteditor != '\0') {
        (*my_texteditor)++;
         len++;
     }
@@ -90,13 +109,18 @@ char *insert (char **my_texteditor)
     return buf;
 }
 
-char *delete1 (char **my_texteditor, unsigned int del)
-{
+/*
+ *************************************************************
+ * API will delete the given set of bytes data from editor   *
+ * and return the buffer to be pushed to stack framework     *                         
+ *************************************************************
+ */
+char *
+delete (char **my_texteditor, unsigned int del) {
     (*my_texteditor)--;
     char *buf = (char *)malloc(del+1);
     unsigned int del1 = del;
-    while(del)
-    {
+    while(del) {
         buf[(del-1)] = **my_texteditor;
         **my_texteditor = '\0';
         (*my_texteditor)--;
@@ -109,6 +133,7 @@ char *delete1 (char **my_texteditor, unsigned int del)
     return buf;
 }
 
+/*API will print the character for the given byte from editor*/
 void print (char *my_texteditor)
 {
     unsigned int print;
@@ -117,6 +142,7 @@ void print (char *my_texteditor)
     return;
 }
 
+/*Main Routine*/
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
     char *my_texteditor = (char *)calloc(1,2*1000000);
@@ -125,24 +151,25 @@ int main() {
     char *buf;
     char *my_texteditor1 = my_texteditor;
     scanf ("%u", &T);
+    /*Number of test cases*/
     while (T)
     {
         scanf ("%u",&choice);
-        if (choice == 1)
+        if (choice == INSERT)
         {
             scanf ("%s", my_texteditor1);
             buf = insert(&my_texteditor1);
             push (&S, 1, buf);
         }
-        else if (choice == 2)
+        else if (choice == DELETE)
         {
             scanf("%u",&del);
             buf = delete1(&my_texteditor1,del);
             push (&S, 2, buf);
         }
-        else if (choice == 3)
+        else if (choice == PRINT)
             print(my_texteditor);
-        else if (choice == 4)
+        else if (choice == UNDO)
             undo (&S, &my_texteditor1);
         T--;
     }
